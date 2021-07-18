9835384ecd289894664008171d8d2034d21d4e38
Guilherme Kruszynski <guilherme.kac10@gmail.com>
2021-07-18 15:55:28 -0300

    adiciona inicio projeto innout

---
+++ innout/public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+require_once(dirname(__FILE__, 2) . '/src/models/User.php');
+
+$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
+print_r($user);
+echo '<br><br>';
+$user->email = 'lucas_alterado@cod3r.com.br';
+print_r($user->email);
---
+++ innout/src/models/Model.php
@@ -0,0 +1,27 @@
+<?php
+
+class Model {
+    protected static $tableName = '';
+    protected static $columns = [];
+    protected $values = [];
+
+    function __construct($arr) {
+        $this->loadFromArray($arr);
+    }
+
+    public function loadFromArray($arr) {
+        if($arr) {
+            foreach($arr as $key => $value) {
+                $this->$key = $value;
+            }
+        }
+    }
+
+    public function __get($key) {
+        return $this->values[$key];
+    }
+
+    public function __set($key, $value) {
+        $this->values[$key] = $value;
+    }
+}
